eb66d77880e1a3a9817978cf82c0efbcab9001fa
Eli-Price <55397706+Eli-Price@users.noreply.github.com>
2023-03-27 17:11:30 -0500

    Added a page where students can see dates and times where they can sign up for presentations, very provisional. (#11)
    * First part of student page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Truman Capstone Scheduling System</h1>
-<a href="/departmentchair/capstonedateselect">Department Chair - Capstone Date Selection</a>
+<body>
+    <p><a href="/departmentchair/capstonedateselect">Department Chair - Capstone Date Selection</a></p>
+    <p><a href="/students/capstonesignups">Student - Capstone Date Selection</a></p>
+</body>
+
+
+
