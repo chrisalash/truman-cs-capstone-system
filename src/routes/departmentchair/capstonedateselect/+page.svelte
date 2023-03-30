bc80eddd258a107ca665ca69d60138ab53acd0e9
GraceHinrichs <73264660+GraceHinrichs@users.noreply.github.com>
2023-03-30 17:03:32 +0000

    Pending changes exported from your codespace

--- src/routes/departmentchair/capstonedateselect/+page.svelte
+++ src/routes/departmentchair/capstonedateselect/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
   const monthsInYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
+  const times = ["12:00","12:15","12:30","12:45","1:00","1:15","1:30","1:45","2:00","2:15","2:30","2:45","3:00","3:15","3:30","3:45","4:00","4:15","4:30","4:45","5:00","5:15","5:30","5:45","6:00"];
+  
   let date = new Date();
   let year = date.getFullYear();
   let month = date.getMonth();  
+  let daysSelected = ["March 3", "March 4", "March 5", "March 9", "March 10"];
+	let numberOfDays= daysSelected.length;
+	
+	
+	
 
   function getDaysInMonth(month: number, year: number) {
     return new Date(year, month + 1, 0).getDate();
@@ -79,6 +85,27 @@
     </tbody>
   </table>
 </div>
+
+<body>
+  <table>
+		<thead>
+      <tr>
+        {#each Object.values(daysSelected) as days}
+					<th>{days}</th>
+				{/each}
+      </tr>
+    </thead>
+    <tbody>
+       {#each Object.values(times) as row}
+    <tr>
+      {#each {length: numberOfDays} as i}
+        <td>{row}</td>
+      {/each}
+    </tr>
+  {/each}
+    </tbody>
+  </table>
+</body>
 
 
 <style>
