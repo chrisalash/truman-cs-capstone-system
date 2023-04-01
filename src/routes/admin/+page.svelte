a4670d552882a98ddf3106acae2cc08725ba795a
jagger-harris <jagger.lee.harris@gmail.com>
2023-04-01 14:53:50 -0500

    Added navbar, refactored components, and updated CSS

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,6 @@
 <main class='flex flex-col'>
   <div class='flex ml-4 mb-8 w-full bg-white rounded p-4'>
-    <h1 class='text-5xl text-violet-800'>Computer Science Capstone Presentation Tools</h1>
+    <h1 class='text-5xl text-violet-800'>Admin Tools</h1>
   </div>
   <div class='flex ml-4 mb-8 w-full bg-white rounded p-4'>
     Put some description here.
