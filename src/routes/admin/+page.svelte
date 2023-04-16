16f07e303677f91182592a576b7f5ef0ab39e15e
jagger-harris <jagger.lee.harris@gmail.com>
2023-04-16 11:31:14 -0500

    Updated Admin Page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,10 +2,16 @@
   <div class='flex ml-4 mb-8 w-full bg-white rounded p-4'>
     <h1 class='text-5xl text-violet-800'>Admin Tools</h1>
   </div>
-  <div class='flex ml-4 mb-8 w-full bg-white rounded p-4'>
-    Put some description here.
+  <div class='flex flex-col ml-4 mb-8 w-full bg-white rounded p-4'>
+    <h1 class='text-2xl text-violet-800'>Warning</h1>
+    This is the administrative access point. If you are not an authorized administrator, please leave now.
   </div>
-  <div class='flex ml-4 mb-8 w-full bg-white rounded p-4'>
-    Another cell if need be.
+  <div class='flex flex-col ml-4 mb-8 w-full bg-white rounded p-4'>
+    <h1 class='text-2xl text-violet-800'>Database Access</h1>
+    Some text.
+  </div>
+  <div class='flex flex-col ml-4 mb-8 w-full bg-white rounded p-4'>
+    <h1 class='text-2xl text-violet-800'>View Logs</h1>
+    Some text.
   </div>
 </main>
