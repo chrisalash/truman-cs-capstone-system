9d0b94d73742a51907acf34618fc4a4e2928b293
jagger-harris <jagger.lee.harris@gmail.com>
2023-04-17 10:58:21 -0500

    Added connected capstone websites

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,20 @@
+<script lang="ts">
+  import { username } from '$lib/stores';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const cookies = document.cookie.split('; ');
+      for (const cookie of cookies) {
+        console.log(cookie)
+        const [name, value] = cookie.split('=');
+        if (name === 'username') {
+          username.set(value);
+        }
+    }
+  }
+);
+</script>
+
 <main class='flex flex-col'>
   <div class='flex ml-4 mb-8 w-full bg-white rounded p-4'>
     <h1 class='text-5xl text-violet-800'>Computer Science Capstone Presentation Tools</h1>
